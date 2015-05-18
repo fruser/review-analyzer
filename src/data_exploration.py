a611180ff2cf43b88b8865a5e31c42666d8547dc
Timur <tgladkih@gmail.com>
2015-05-18 16:47:33 -0400

    added json output to data_exploration

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -1,13 +1,15 @@
 __author__ = 'Timur Gladkikh'
 from collections import defaultdict
+import json
 from data_loader import *
 
 def users_reviews_count(reviews):
+    path = '../results/user_review.json'
     usr_review = defaultdict(int)
     for i in range(0, len(reviews)):
         review = reviews['{0}'.format(i)]
         usr_review[review['reviewerID']] += 1
-    return usr_review
+    json.dump(usr_review, open(path, 'w'))
 
 
 def reviews_count(reviews):
@@ -17,9 +19,8 @@ def reviews_count(reviews):
 def main():
     path = '../data/sample_file.json.gz'
     reviews = parse(path)
-    #usr_review = users_reviews_count(reviews)      #Result => 2088647
-    reviews_count(reviews)                          #Result => 4628130
-
+    users_reviews_count(reviews)                      #Result => 2088647
+    reviews_count(reviews)                            #Result => 4628130
 
 if __name__ == '__main__':
     main()
