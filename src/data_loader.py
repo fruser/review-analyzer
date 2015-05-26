fbb1f734f21e02a788fb79633e97033262e270a3
Timur <tgladkih@gmail.com>
2015-05-26 01:17:03 -0400

    replaced pickle to JSON

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,32 +2,48 @@ __author__ = 'Timur Gladkikh'
 import gzip
 import os
 import pickle
+import json
+
+DATA_FILE = '../data/reviews_Movies_and_TV.json.gz'
 
 def parse():
-    path = '../data/reviews_Movies_and_TV.json.gz'
-    pickled_file = '../data/pickled/reviews.pickle'
-    if os.path.isfile(pickled_file):
-        return load_pickle(pickled_file)
+    json_file = '../data/json/reviews.json'
+    if os.path.isfile(json_file):
+        return load_json(json_file)
 
     line_num = 0
     parsed = {}
-    with gzip.open(path, 'rb') as f:
+    with gzip.open(DATA_FILE, 'rb') as f:
         for line in f:
             parsed['{0}'.format(line_num)] = eval(line)
             line_num += 1
 
-    dump_pickle(parsed, pickled_file)
+    dump_json(parsed, json_file)
     return parsed
 
-def dump_pickle(obj, file):
-    if not os.path.isfile(file):
-        with open(file, 'wb') as f:
-            pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+def parse_strict_json():
+    json_file = '../data/json/reviews_strict.json'
+    if os.path.isfile(json_file):
+        return load_json(json_file)
 
-def load_pickle(file):
+    line_num = 0
+    parsed = {}
+    with gzip.open(DATA_FILE, 'rb') as f:
+        for line in f:
+            yield json.dump(eval(line))
+
+    dump_json(parsed, json_file)
+    return parsed
+
+def dump_json(obj, file):
+    if not os.path.isfile(file):
+        with open(file, 'w') as f:
+            json.dump(obj, f, indent=2, sort_keys=True)
+
+def load_json(file):
     if os.path.isfile(file):
-        with open(file, 'rb') as f:
-            data = pickle.load(f)
+        with open(file, 'r') as f:
+            data = json.load(f)
         return data
 
 def main():
--- src/utils.py
+++ src/utils.py
@@ -3,18 +3,32 @@ from collections import defaultdict
 from operator import itemgetter
 from data_loader import *
 
-def get_review_sample():
-    pickled_file = '../data/pickled/review_sample.pickle'
-    if os.path.isfile(pickled_file):
-        return load_pickle(pickled_file)
+def get_review_sample(sample_size=100000, condition=20):
+    json_file = '../data/json/review_sample.json'
+    if os.path.isfile(json_file):
+        return load_json(json_file)
 
     parsed_file = parse()
-    review_count = get_movie_vs_reviews_count(parsed_file)
-    parsed_file = None   # free up memory of parsed_file
-    review_count_filtered = list(review_count[x][0] for x in range(0, len(review_count)) if review_count[x][1] >= 10)
 
-    pass
+    count_filtered = '../data/json/review_count_filtered.json'
+    if os.path.isfile(count_filtered):
+        review_count_filtered = load_json(count_filtered)
+    else:
+        review_count = get_users_vs_reviews_count(parsed_file)
+        review_count_filtered = list(review_count[x][0]
+                                     for x in range(0, len(review_count)) if review_count[x][1] >= condition)
+        dump_json(review_count_filtered, '../data/json/review_count_filtered.json')
 
+    filter_list = []
+    sample = {}
+    for i in range(0, len(parsed_file)):
+        if parsed_file['{0}'.format(i)]['reviewerID'] in review_count_filtered:
+            sample['{0}'.format(i)] = parsed_file['{0}'.format(i)]
+            filter_list.append(i)
+
+    dump_json(sample, json_file)  # needs re-work
+    dump_json(filter_list, '../data/json/filter_list.json')  # temporary
+    return sample
 
 def get_users_vs_reviews_count(reviews, sort=True):
     return get_count(reviews, 'reviewerID', 'usr_review_count', sort)
@@ -22,10 +36,10 @@ def get_users_vs_reviews_count(reviews, sort=True):
 def get_movie_vs_reviews_count(reviews, sort=True):
     return get_count(reviews, 'asin', 'movie_review_count', sort)
 
-def get_count(reviews, obj, pickle_name, sort=True):
-    pickled_file = '../data/pickled/{0}.pickle'.format(pickle_name)
-    if os.path.isfile(pickled_file):
-        return load_pickle(pickled_file)
+def get_count(reviews, obj, json_name, sort=True):
+    json_file = '../data/json/{0}.json'.format(json_name)
+    if os.path.isfile(json_file):
+        return load_json(json_file)
 
     review_count = defaultdict(int)
     for i in range(0, len(reviews)):
@@ -35,11 +49,8 @@ def get_count(reviews, obj, pickle_name, sort=True):
     if sort:
         review_count = sorted(review_count.items(), key=itemgetter(1), reverse=True)  # saves data as tuple
 
-    dump_pickle(review_count, pickled_file)
+    dump_json(review_count, json_file)
     return review_count
-
-def get_reviews_count(reviews):
-    print(len(reviews))
 
 def main():
     pass
